1c14d2f7c21b9de287a6b41d4cc7cf0b8adfb6b2
arsenbelarus <arsenbelarus@gmail.com>
2020-06-10 09:02:13 +0300

    Home task 3 added controlled input and onKeyPressHandler

---
+++ src/input/Input.tsx
@@ -0,0 +1,36 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {v1} from "uuid";
+import s from './Input.module.css';
+
+function Input () {
+
+    let [names, setNames] = useState<Array<string>>([])
+
+    const addName = (nameValue:string) => {
+        let newNames = [...names, nameValue]
+        setNames (newNames);
+    }
+
+    let [input, setInput] = useState<string>("")
+
+    const inputHandler = () => {
+        if (input) {
+            addName(input);
+            alert (`Hello ${input}. How are you?`)
+            setInput("");
+        } else {alert ("Please fill in your name")}
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setInput(e.currentTarget.value)
+    const onKeyPressHandler = (e:KeyboardEvent) => { if (e.charCode === 13) {inputHandler()} }
+
+    return (
+        <div className={s.inputContainer}>
+            <input className={s.input} type="text" placeholder={"Enter your name here"}
+                   value={input} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
+            <button className={s.button} onClick={inputHandler}>Add</button>
+            <span className={s.span}> {`Total amount of names is ${names.length}`} </span>
+        </div>
+    )
+}
+
+export default Input
